73190fe8a87788b929521c6fbbdb8034237a0e18
asalcedod <asalcedo@fundacionidi.org>
2019-05-27 16:08:41 -0500

    weather

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import WeatherLocation from './components/WeatherLocation';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,16 +10,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello Word.
+            <WeatherLocation></WeatherLocation>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
---
+++ src/components/WeatherTemperature.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import WeatherIcons from 'react-weathericons';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY
+} from './../constants/weathers';
+
+const icons = {
+    [CLOUD]: "cloud",
+    [CLOUDY]: "cloudy",
+    [SUN]: "sun",
+    [RAIN]: "rain",
+    [SNOW]: "snow",
+    [WINDY]: "windy",
+};
+
+const getWeatherIcon = weatherState => {
+    const icon = icons[weatherState];
+    if(icon)
+        return <WeatherIcons name={icon} size="2x"></WeatherIcons>
+    else
+        return <WeatherIcons name={"day-sunny"} size="2x"></WeatherIcons>
+}   
+
+const WeatherTemperature = ({ temperature, weatherState }) => (
+
+    <div>
+        {
+            getWeatherIcon(weatherState)
+        }
+        <span>
+            {`${temperature} C°`}
+        </span>
+    </div>
+);
+
+export default WeatherTemperature;
